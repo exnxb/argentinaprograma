5b4b59960a60919bb04cafc286be61c785733092
exnxb <nezov.ildareb@gmail.com>
2023-05-25 12:15:00 -0300

    Agrego ejercicio 2

---
+++ areaRectangulo.js
@@ -0,0 +1,13 @@
+// parte a
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log (area);
+
+
+
+// parte b
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log (area);
