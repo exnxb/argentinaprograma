bf5dff886b3740240ae33542935a415ca07da51e
exnxb <nezov.ildareb@gmail.com>
2023-05-26 17:13:35 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,10 @@
+// parte a
+let tempFahren = 50;
+let tempCels = (tempFahren - 32)*(5/9);
+console.log (tempCels);
+
+
+// parte b
+tempFahren = 70
+tempCels = (tempFahren - 32)*(5/9);
+console.log (tempCels)
