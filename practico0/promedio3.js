de30939542e7680bc15c867ec62fd58ab39c77e6
exnxb <nezov.ildareb@gmail.com>
2023-05-26 17:15:21 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,20 @@
+// parte a
+let num1 = 5
+let num2 = 7
+let num3 = 9
+let promedio = (num1 + num2 + num3)/3
+console.log (promedio)
+
+// parte b
+num1 = -2
+num2 = -3
+num3 = -8
+promedio = (num1 + num2 + num3)/3
+console.log (promedio)
+
+// parte c
+num1 = -1.2
+num2 = 5.3
+num3 = -3.3
+promedio = (num1 + num2 + num3)/3
+console.log (promedio)
