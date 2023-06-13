6d6e8dc59031abbd5fdcb30a3e6251d210527e4f
exnxb <134018088+exnxb@users.noreply.github.com>
2023-06-13 20:35:37 -0300

    PARTE 2: Manejo de arreglos (item 5)

---
+++ practico1/frutas.js
@@ -0,0 +1,17 @@
+//a
+let frutas = [];
+//b
+frutas [0] = "manzana"
+frutas [1] = "banana"
+frutas [2] = "pera"
+frutas [3] = "naranja"
+frutas [4] = "sandía"
+console.log (frutas);
+//c
+let ultima = frutas [4]
+console.log (ultima)
+console.log ("La ultima fruta agregada fue: " + ultima);
+//d
+ let primera = frutas [0]
+ console.log (primera)
+ console.log ("La primera fruta agregada fue: " + primera)
